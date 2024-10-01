a51cea132ce0e1160e99393436782c92af849254
MuhamamdRafliii <raflimuhh09@gmail.com>
2024-10-01 19:33:39 +0700

    component-style1

--- App.tsx
+++ App.tsx
@@ -1,25 +1,45 @@
-import 'react-native-gesture-handler'; // Ensure this is at the top
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
-import ImageScreen from './src/screens/ImageScreen';
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
-// Create the stack navigator
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Image">
-        <Stack.Screen
-          name="Image"
-          component={ImageScreen}
-          options={{ title: 'My App' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+class App extends Component {
+  constructor(props:any) {
+    super(props);
+    this.state = { }; 
+  }
+  render() {
+    return (
+    <View>
+      <Text style={{color: 'blue'}}>Hello Cuy</Text>
+      <Text
+      style = {{
+        fontSize: 28,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        textAlign: 'center'
+        }}>
+          Hello Cuy2
+      </Text>
+      <Text
+      style = {{
+        color: '#DC143C',
+        fontSize: 28,
+        textDecorationLine: 'line-through',
+        textAlign: 'left'
+      }}>
+        Rp.20.000,00
+      </Text>
+      <Text
+      style = {{
+        color: 'rgb(0, 0, 0)',
+        fontSize: 28,
+        textDecorationLine: 'underline',
+        textAlign: 'right'
+      }}>
+        Rp.10.000,00
+      </Text>
+    </View>
+    )
+  }
+}
 
 export default App;
