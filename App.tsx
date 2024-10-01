8bcb35d3290c6ffe8b0375c25fc9e39bea7a84a7
MuhamamdRafliii <raflimuhh09@gmail.com>
2024-10-01 23:14:11 +0700

    switch-textinput

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import {  View, Text, StatusBar, Image, TouchableOpacity } from 'react-native';
+import {  View, Text, StatusBar, Image, TouchableOpacity, Switch, TextInput } from 'react-native';
 
-class App extends Component {
+// Define state interface
+interface AppState {
+  header: string,
+  value: boolean,
+  username: string,
+}
+
+class App extends Component<{}, AppState> {
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      header: 'Home',
+      value: true,
+      username: ''
+    };
   }
   render() {
     return (
@@ -23,7 +34,7 @@ class App extends Component {
             fontWeight: 'bold',
             fontSize: 25
             }}>
-              Header
+              {this.state.header}
           </Text>
         </View>
         <View style={{
@@ -34,6 +45,29 @@ class App extends Component {
           style={{width: 300, height: 300}}
           />
         </View>
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 20,
+        }}>
+          <Switch value={this.state.value} onValueChange={() => this.setState({
+            value : !this.state.value
+            })}>
+          </Switch>
+        </View>
+        <TextInput
+          value={this.state.username}
+          style={{
+            borderWidth: 1,
+            borderColor: '#03fc98',
+            marginHorizontal: 20,
+            paddingHorizontal: 10,
+            borderRadius: 3,
+            marginTop: 10,
+            color: 'black'
+          }}
+          onChangeText={value => this.setState({username: value})}
+        />
         <TouchableOpacity
         style={{
           backgroundColor: '#03fc98',
@@ -50,7 +84,7 @@ class App extends Component {
             justifyContent: 'center',
             fontWeight: 'bold',
             fontSize: 25,
-          }} onPress={() => console.log('Klik')}>
+          }} onPress={() => console.log('Halo Rafli')}>
             Click Here!
           </Text>
         </TouchableOpacity>
